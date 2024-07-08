8b036f29c898a14266dac5dc6b28b0260c9463ea
Lukical <rszynski@gmail.com>
2024-07-08 18:59:49 -0300

    Recebendo produtos e comencando inicio

--- app/src/pages/Inicio/index.tsx
+++ app/src/pages/Inicio/index.tsx
@@ -1,16 +1,40 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './Inicio.module.scss';
+import http from '../../data/http';
+import IProduto from '../../types/IProduto';
 
-function Inicio(){
+interface IPromise{
+    products: IProduto[],
+    total: number,
+    totalPages: number
+}
+
+const Inicio = () =>{
+
+    const [produtos, setProdutos] = useState<IProduto[]>([]);
+    const [total, setTotal] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
 
     useEffect(()=>{
-        
+        http.get<IPromise>("products")
+        .then(response =>{
+            setProdutos(response.data.products);
+            setTotal(response.data.total);
+            setTotalPages(response.data.totalPages);
+        })
+        .catch(e=>{
+            console.log(e)
+        })
     },[])
 
     return(
         <section className={styles.container}>
             <div className={styles.containerProducts}>
-                
+                {produtos.map(produto =>
+                    <div>
+                        <li>{produto.name}</li>
+                    </div>
+                )}
             </div>
         </section>
     );
---
+++ app/src/types/IProduto.ts
@@ -0,0 +1,11 @@
+interface IProduto{
+    id: number,
+    name: string,
+    brand: string,
+    description: string,
+    price: number,
+    img: string,
+    createdAt: string,
+    updatedAt: string
+}
+export default IProduto;
